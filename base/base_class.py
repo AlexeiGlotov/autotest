7c6609e4de3d27f037b29ba3cff997fe935efd90
Alexei Glotov <tnffx@icloud.com>
2023-11-18 22:04:47 +0300

    basefix

--- base/base_class.py
+++ base/base_class.py
@@ -5,34 +5,42 @@ class Base():
         self.driver = driver
         self.action = action
 
+    """Получение URL в моменте"""
+
     def get_current_url(self):
         return self.driver.current_url
     
+    """Проверка двух строк"""
+
     def assert_word(self,word,result):
         assert word == result
         print(f'[GOOD] assert_word {word} == {result}')
 
-
+    """Проверка URL в бразуере с URL из строки"""
     def assert_url(self,url):
         assert self.driver.current_url == url
         print(f'[GOOD] assert_url {self.driver.current_url} == {url}')
   
-    
+    """Аналогично выше функции , но проверят как подстроку."""
+
     def assert_url_part(self,url):
         result = self.driver.current_url.find(url)
         assert result != -1
         print(f'[GOOD] assert_url {self.driver.current_url} == {url}')
 
+    """Проверка что выбран checkbox или radio button"""
 
     def checked_radio_checkbox(self,element,expectation):
         assert expectation == element.is_selected()
         print(f'[GOOD] checked_radio_checkbox') 
 
+    """Установить фокус на конкретный элемент"""
 
     def move_to_element(self,elem):
         self.action.move_to_element(elem).perform()
         print(f'[GOOD] move_to_element')
 
+    """Выдрать из строки только цифры"""
 
     def isDigit(self,text):
         stri = ''
@@ -42,6 +50,8 @@ class Base():
 
         return stri
     
+    """Сделать скриншот"""
+
     def screenshot(self,moduls):
         self.driver.save_screenshot(f'C:\\Users\\Alexei\\vsempodarok\\screen\\{moduls}-{time.time()}.png')
            
